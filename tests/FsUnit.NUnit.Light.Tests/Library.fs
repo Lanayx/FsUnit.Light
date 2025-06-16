d03692e4f92bd0c141e5e4ba007c20cadebe16de
Vladimir Shchur <odindafna2006@rambler.ru>
2025-06-15 19:02:30 -0700

    Simplified test structure

--- tests/FsUnit.NUnit.Light.Tests/Library.fs
+++ tests/FsUnit.NUnit.Light.Tests/Library.fs
@@ -6,25 +6,13 @@ open FsUnit.NUnit.Light
 module ShouldBeEmptyTests =
 
     [<Test>]
-    let ``empty List should be Empty``() =
+    let ``empty collections should be Empty``() =
         [] |> shouldBeEmpty
-
-    [<Test>]
-    let ``non-empty List should fail to be Empty``() =
-        shouldFail<AssertionException>(fun () -> [ 1 ] |> shouldBeEmpty)
-
-    [<Test>]
-    let ``empty Array should be Empty``() =
         [||] |> shouldBeEmpty
-
-    [<Test>]
-    let ``non-empty Array should fail to be Empty``() =
-        shouldFail<AssertionException>(fun () -> [| 1 |] |> shouldBeEmpty)
-
-    [<Test>]
-    let ``empty Seq should be Empty``() =
         Seq.empty |> shouldBeEmpty
 
     [<Test>]
-    let ``non-empty Seq should fail to be Empty``() =
+    let ``non-empty collections fail to be Empty``() =
+        shouldFail<AssertionException>(fun () -> [ 1 ] |> shouldBeEmpty)
+        shouldFail<AssertionException>(fun () -> [| 1 |] |> shouldBeEmpty)
         shouldFail<AssertionException>(fun () -> seq { 1 } |> shouldBeEmpty)
--- tests/FsUnit.Xunit.Light.Tests/Library.fs
+++ tests/FsUnit.Xunit.Light.Tests/Library.fs
@@ -7,25 +7,13 @@ open FsUnit.Xunit.Light
 module ShouldBeEmptyTests =
 
     [<Fact>]
-    let ``empty List should be Empty``() =
+    let ``empty collections should be Empty``() =
         [] |> shouldBeEmpty
-
-    [<Fact>]
-    let ``non-empty List should fail to be Empty``() =
-        shouldFail<EmptyException>(fun () -> [ 1 ] |> shouldBeEmpty)
-
-    [<Fact>]
-    let ``empty Array should be Empty``() =
         [||] |> shouldBeEmpty
-
-    [<Fact>]
-    let ``non-empty Array should fail to be Empty``() =
-        shouldFail<EmptyException>(fun () -> [| 1 |] |> shouldBeEmpty)
-
-    [<Fact>]
-    let ``empty Seq should be Empty``() =
         Seq.empty |> shouldBeEmpty
 
     [<Fact>]
-    let ``non-empty Seq should fail to be Empty``() =
+    let ``non-empty collections fail to be Empty``() =
+        shouldFail<EmptyException>(fun () -> [ 1 ] |> shouldBeEmpty)
+        shouldFail<EmptyException>(fun () -> [| 1 |] |> shouldBeEmpty)
         shouldFail<EmptyException>(fun () -> seq { 1 } |> shouldBeEmpty)
