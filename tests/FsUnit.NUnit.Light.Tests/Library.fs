797efe863a675028d413eb2b87b0ecb8ad17ff32
Vladimir Shchur <odindafna2006@rambler.ru>
2025-06-15 19:17:34 -0700

    More test cases added

--- tests/FsUnit.NUnit.Light.Tests/Library.fs
+++ tests/FsUnit.NUnit.Light.Tests/Library.fs
@@ -16,3 +16,45 @@ module ShouldBeEmptyTests =
         shouldFail<AssertionException>(fun () -> [ 1 ] |> shouldBeEmpty)
         shouldFail<AssertionException>(fun () -> [| 1 |] |> shouldBeEmpty)
         shouldFail<AssertionException>(fun () -> seq { 1 } |> shouldBeEmpty)
+
+module ShouldHaveLengthTests =
+
+    [<Test>]
+    let ``collections with expected length should pass``() =
+        [ 1 ] |> shouldHaveLength 1
+        seq { 1 } |> shouldHaveLength 1
+        "a" |> shouldHaveLength 1
+
+    [<Test>]
+    let ``collections with unexpected length fail``() =
+        shouldFail<AssertionException>(fun () -> [] |> shouldHaveLength 1)
+        shouldFail<AssertionException>(fun () -> seq {} |> shouldHaveLength 1)
+        shouldFail<AssertionException>(fun () -> "" |> shouldHaveLength 1)
+
+module ShouldBeGreaterThanTests =
+
+    [<Test>]
+    let ``shouldBeGreaterThan passes for greater values``() =
+        2 |> shouldBeGreaterThan 1
+        "b" |> shouldBeGreaterThan "a"
+        2.0 |> shouldBeGreaterThan 1.0
+
+    [<Test>]
+    let ``shouldBeGreaterThan fails for lesser or equal values``() =
+        shouldFail<AssertionException>(fun () -> 1 |> shouldBeGreaterThan 2)
+        shouldFail<AssertionException>(fun () -> "a" |> shouldBeGreaterThan "a")
+        shouldFail<AssertionException>(fun () -> 1.0 |> shouldBeGreaterThan 2.0)
+
+module ShouldBeSmallerThanTests =
+
+    [<Test>]
+    let ``shouldBeSmallerThan passes for smaller values``() =
+        1 |> shouldBeSmallerThan 2
+        "a" |> shouldBeSmallerThan "b"
+        1.0 |> shouldBeSmallerThan 2.0
+
+    [<Test>]
+    let ``shouldBeSmallerThan fails for greater or equal values``() =
+        shouldFail<AssertionException>(fun () -> 2 |> shouldBeSmallerThan 1)
+        shouldFail<AssertionException>(fun () -> "a" |> shouldBeSmallerThan "a")
+        shouldFail<AssertionException>(fun () -> 2.0 |> shouldBeSmallerThan 1.0)
--- tests/FsUnit.Xunit.Light.Tests/Library.fs
+++ tests/FsUnit.Xunit.Light.Tests/Library.fs
@@ -17,3 +17,45 @@ module ShouldBeEmptyTests =
         shouldFail<EmptyException>(fun () -> [ 1 ] |> shouldBeEmpty)
         shouldFail<EmptyException>(fun () -> [| 1 |] |> shouldBeEmpty)
         shouldFail<EmptyException>(fun () -> seq { 1 } |> shouldBeEmpty)
+
+module ShouldHaveLengthTests =
+
+    [<Fact>]
+    let ``collections with expected length should pass``() =
+        [ 1 ] |> shouldHaveLength 1
+        seq { 1 } |> shouldHaveLength 1
+        "a" |> shouldHaveLength 1
+
+    [<Fact>]
+    let ``collections with unexpected length fail``() =
+        shouldFail<EqualException>(fun () -> [] |> shouldHaveLength 1)
+        shouldFail<EqualException>(fun () -> seq {} |> shouldHaveLength 1)
+        shouldFail<EqualException>(fun () -> "" |> shouldHaveLength 1)
+
+module ShouldBeGreaterThanTests =
+
+    [<Fact>]
+    let ``shouldBeGreaterThan passes for greater values``() =
+        2 |> shouldBeGreaterThan 1
+        "b" |> shouldBeGreaterThan "a"
+        2.0 |> shouldBeGreaterThan 1.0
+
+    [<Fact>]
+    let ``shouldBeGreaterThan fails for lesser or equal values``() =
+        shouldFail<TrueException>(fun () -> 1 |> shouldBeGreaterThan 2)
+        shouldFail<TrueException>(fun () -> "a" |> shouldBeGreaterThan "a")
+        shouldFail<TrueException>(fun () -> 1.0 |> shouldBeGreaterThan 2.0)
+
+module ShouldBeSmallerThanTests =
+
+    [<Fact>]
+    let ``shouldBeSmallerThan passes for smaller values``() =
+        1 |> shouldBeSmallerThan 2
+        "a" |> shouldBeSmallerThan "b"
+        1.0 |> shouldBeSmallerThan 2.0
+
+    [<Fact>]
+    let ``shouldBeSmallerThan fails for greater or equal values``() =
+        shouldFail<TrueException>(fun () -> 2 |> shouldBeSmallerThan 1)
+        shouldFail<TrueException>(fun () -> "a" |> shouldBeSmallerThan "a")
+        shouldFail<TrueException>(fun () -> 2.0 |> shouldBeSmallerThan 1.0)
