549bdd6635425fb8fc973465d5f184622490e081
Vladimir Shchur <odindafna2006@rambler.ru>
2025-06-15 18:52:41 -0700

    xUnit and NUnit added with first test module

---
+++ src/FsUnit.Xunit.Light/Library.fs
@@ -0,0 +1,39 @@
+﻿namespace FsUnit.Xunit.Light
+
+open Xunit
+
+[<AutoOpen>]
+module TopLevelOperators =
+
+    let shouldEqual<'a> (expected: 'a) (actual: 'a) =
+        Assert.Equal(expected, actual)
+
+    let shouldNotEqual<'a> (expected: 'a) (actual: 'a) =
+        Assert.NotEqual(expected, actual)
+
+    let shouldContain<'a when 'a: equality> (expected: 'a) (actual: 'a seq) =
+        Assert.Contains(expected, actual)
+
+    let shouldBeEmpty<'a>(actual: 'a seq) =
+        Assert.Empty actual
+
+    let shouldNotContain<'a when 'a: equality> (expected: 'a) (actual: 'a seq) =
+        Assert.DoesNotContain(expected, actual)
+
+    let shouldBeSmallerThan<'a when 'a: comparison> (expected: 'a) (actual: 'a) =
+        Assert.True(actual < expected)
+
+    let shouldBeGreaterThan<'a when 'a: comparison> (expected: 'a) (actual: 'a) =
+        Assert.True(actual > expected)
+
+    let shouldFail<'exn when 'exn :> exn>(f: unit -> unit) =
+        Assert.Throws<'exn>(f) |> ignore
+
+    let shouldContainText (expected: string) (actual: string) =
+        Assert.Contains(expected, actual)
+
+    let shouldNotContainText (expected: string) (actual: string) =
+        Assert.DoesNotContain(expected, actual)
+
+    let shouldHaveLength<'a> (expected: int) (actual: 'a seq) =
+        Assert.Equal(expected, Seq.length actual)
---
+++ tests/FsUnit.Xunit.Light.Tests/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace FsUnit.Xunit.Light.Tests
+
+open Xunit
+open Xunit.Sdk
+open FsUnit.Xunit.Light
+
+module ShouldBeEmptyTests =
+
+    [<Fact>]
+    let ``empty List should be Empty``() =
+        [] |> shouldBeEmpty
+
+    [<Fact>]
+    let ``non-empty List should fail to be Empty``() =
+        shouldFail<EmptyException>(fun () -> [ 1 ] |> shouldBeEmpty)
+
+    [<Fact>]
+    let ``empty Array should be Empty``() =
+        [||] |> shouldBeEmpty
+
+    [<Fact>]
+    let ``non-empty Array should fail to be Empty``() =
+        shouldFail<EmptyException>(fun () -> [| 1 |] |> shouldBeEmpty)
+
+    [<Fact>]
+    let ``empty Seq should be Empty``() =
+        Seq.empty |> shouldBeEmpty
+
+    [<Fact>]
+    let ``non-empty Seq should fail to be Empty``() =
+        shouldFail<EmptyException>(fun () -> seq { 1 } |> shouldBeEmpty)
